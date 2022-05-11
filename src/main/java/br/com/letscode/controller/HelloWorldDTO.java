66964f209e024561283f2d8aec5dd56e05e55625
Lorraine Letiele Pies <lorraine.letielea@vitat.com.br>
2022-05-11 09:59:30 -0300

    alteracoes durante a aula 2

--- src/main/java/br/com/letscode/controller/HelloWorldDTO.java
+++ src/main/java/br/com/letscode/controller/HelloWorldDTO.java
@@ -1,16 +1,18 @@
 package br.com.letscode.controller;
 
-import lombok.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+//java bean: construtor padrao, get/sets
 @NoArgsConstructor
 @Data
 public class HelloWorldDTO {
 
-    private String mensagem;
+    //private String mensagem;
     private String username;
-    private LocalDate data;
+    //private LocalDateTime data;
+    //private String corPreferida;
     private Integer idade;
 }
